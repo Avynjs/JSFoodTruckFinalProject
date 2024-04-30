c626f82796255980f4ba9401475940c7916a30d7
Summer Ruff <summerruff13@gmail.com>
2024-04-29 20:23:41 -0500

    completed home page

---
+++ public/site.js
@@ -0,0 +1,21 @@
+function displayText(textID) {
+    var text = document.getElementById(textID);
+    text.style.display = "block";
+
+    // var showBtn = document.getElementById("drop");
+    // var hideBtn = document.getElementById("hide");
+
+    // showBtn.classList.add("hide");
+    // hideBtn.classList.add("show");
+}
+
+function hideText(textID) {
+    var text = document.getElementById(textID);
+    text.style.display = "none";
+
+    // var showBtn = document.getElementById("drop");
+    // var hideBtn = document.getElementById("hide");
+
+    // showBtn.classList.add("show");
+    // hideBtn.classList.add("hide");
+}
