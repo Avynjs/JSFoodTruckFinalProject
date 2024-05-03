7d82e5783b4a31a03e2dfe5bfaebe64811a1146b
Amelia <paytonlederhaus@gmail.com>
2024-05-03 14:42:06 -0500

    Added GET events

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.static('public'))
-app.use('/api/menu', require('./routes/api-routes'))
+app.use('/api', require('./routes/api-routes'))
 app.use(require('./routes/static'))
 
 const message = `Server running: http://localhost:${port}`
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,12 +2,19 @@ const router = require('express').Router()
 const { getCollection, ObjectId } = require('../foodtruck-db')
 
 // GET /api/menu (getting all menu items)
-router.get('/', async (req, res) => {
+router.get('/menu', async (req, res) => {
     const collection = await getCollection('foodtruck-api', 'menu')
     const menu = await collection.find().toArray()
 
 	res.json(menu)
 })
 
+// GET /api/events (get all events)
+router.get('/events', async (req, res) => {
+    const collection = await getCollection('foodtruck-api', 'events')
+    const events = await collection.find().toArray()
+
+    res.json(events)
+})
 
 module.exports = router
