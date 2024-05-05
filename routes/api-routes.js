1448b71e7a34c4d64719329238e354bcba190a44
Avynjs <500197118@fvtc.edu>
2024-05-05 15:49:14 -0500

    finished POST /api/events, GET /api/events/:id, PUT api/events/:id, and DELETE /api/events/:id

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { request } = require('express')
 const { getCollection, ObjectId } = require('../foodtruck-db')
 
 // GET /api/menu (getting all menu items)
@@ -17,25 +18,42 @@ router.get('/events', async (req, res) => {
     res.json(events)
 })
 
-//POST /api/events (create a new event)
-router.post('/events', async (req, res) => {
-    const collection = await getCollection('foodtruck-api', 'events')
-    const event = req.body
-
-    await collection.insertOne(event)
-
-    res.json({ message: 'Event created' })
-})
-
 // GET /api/events/:id (get a single event)
-router.get('/api/events/:id', async (req, res) => {
-    const { id } = req.params.id
+router.get('/events/:id', async (req, res) => {
+    const { id } = req.params
     const collection = await getCollection('foodtruck-api', 'events')
-    const event = await collection.findOne({ "_id": ObjectId(id) })
-
+    const event = await collection.findOne({_id: new ObjectId(id)})
     res.json(event)
 })
 
+//POST /api/events (create a new event)
+router.post('/events', async (req, res) => {
+const { body } = req
+const { name, date } = body
+const event = { name, date }
 
+const collection = await getCollection('foodtruck-api', 'events')
+const result = await collection.insertOne(event)
+res.json(result)
+})
+
+// PUT /api/events/:id (update a single event)
+router.put('/events/:id', async (req, res) => {
+    const { body, params } = req
+    const { id } = params
+    const { name, date } = body
+    const event = { name, date }
+
+    const collection = await getCollection('foodtruck-api', 'events')
+    const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: event })
+    res.json(result)
+})
+
+router.delete('/events/:id', async (req, res) => {
+    const { id } = req.params
+    const collection = await getCollection('foodtruck-api', 'events')
+    const result = await collection.deleteOne({ _id: new ObjectId(id) })
+    res.json(result)
+})
 
 module.exports = router
