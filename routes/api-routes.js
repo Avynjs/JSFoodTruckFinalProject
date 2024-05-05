0fdaa32a97b05048d5eb34cf538b183ce40929df
Avynjs <500197118@fvtc.edu>
2024-05-05 12:39:52 -0500

    created post, tried creating get for id but cant get it working right

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,4 +17,25 @@ router.get('/events', async (req, res) => {
     res.json(events)
 })
 
+//POST /api/events (create a new event)
+router.post('/events', async (req, res) => {
+    const collection = await getCollection('foodtruck-api', 'events')
+    const event = req.body
+
+    await collection.insertOne(event)
+
+    res.json({ message: 'Event created' })
+})
+
+// GET /api/events/:id (get a single event)
+router.get('/api/events/:id', async (req, res) => {
+    const { id } = req.params.id
+    const collection = await getCollection('foodtruck-api', 'events')
+    const event = await collection.findOne({ "_id": ObjectId(id) })
+
+    res.json(event)
+})
+
+
+
 module.exports = router
